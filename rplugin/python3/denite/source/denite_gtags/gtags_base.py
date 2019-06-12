525ac18958bced5146d4c1eefc775ff24ebc0841
王彦金 <wyj0056@175game.com>
2019-06-12 16:41:54 +0800

    Adjust to use gen_tags config.

--- rplugin/python3/denite/source/denite_gtags/gtags_base.py
+++ rplugin/python3/denite/source/denite_gtags/gtags_base.py
@@ -4,6 +4,19 @@ from denite.source.base import Base  # pylint: disable=locally-disabled, import-
 import denite.util  # pylint: disable=locally-disabled, import-error
 
 
+def get_subprocess_env(vim):
+    """ Determine whether gen_tags gtags is in use, if so fit to its config.
+    :returns: env
+
+    """
+    if vim.call("exists", "g:loaded_gentags#gtags"):
+        if vim.call("eval", "g:loaded_gentags#gtags"):
+            project_root_path = vim.call("$GTAGSROOT")
+            project_tags_path = vim.call("$GTAGSDBPATH")
+            return {"GTAGSROOT": project_root_path, "GTAGSDBPATH": project_tags_path}
+    return {}
+
+
 class GtagsBase(Base):
     def gather_candidates(self, context):
         self.word = self._get_search_word(context)
@@ -41,7 +54,8 @@ class GtagsBase(Base):
             universal_newlines=True,
             stdin=subprocess.PIPE if input else None,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+            stderr=subprocess.PIPE,
+            env=get_subprocess_env(self.vim))
         try:
             output, error = global_proc.communicate(input=input, timeout=15)
         except subprocess.TimeoutExpired:
